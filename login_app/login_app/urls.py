5378c16cdb3738957f3202372693eae1f5c32e56
Shivam Shrirao <shivamshrirao@gmail.com>
2020-03-03 16:10:59 +0530

    app url fix

--- login_app/login_app/urls.py
+++ login_app/login_app/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('myapp.urls')),
     path('polls/', include('polls.urls')),
-    path('',include('polls.urls'))
+    path('',lambda r: HttpResponseRedirect('polls/'))
 ]
